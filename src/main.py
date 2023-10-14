5ed55dd637f898a3bbfde24bcc0dac94f6636f13
user <email>
2023-10-14 16:42:04 -0400

    move file check functions to manager class

--- src/main.py
+++ src/main.py
@@ -1,34 +1,33 @@
-import csv
-import time
 
 # MODULES
 from subject.subject import Subject
 from teacher.teacher import Teacher
 from xscheduly_manager.xscheduly_manager import XschedulyManager
 
+manager = None
+
 def main():
     try:
-        result = verify_file_format("teachers_and_subjects.csv")
+        instantiate_modules()
+
+    except Exception as e:
+        print("An error occurred:", str(e))
+
+    try:
+        result = manager.verify_file_format("teachers_and_subjects.csv")
         print(f"[{result}]")
 
     except Exception as e:
         print("An error occurred:", str(e))
 
-def verify_file_format(file_path):
-    print("Checking file format", end="")
-    for _ in range(10):
-        print(".", end="", flush=True)
-        time.sleep(0.025)
 
-    if validate_csv_file(file_path):
-        return "OK"
-    else:
-        return "FORMAT_ERROR"
 
-def validate_csv_file(file_path):
-    # Add your CSV validation logic here
-    # Return True if valid, False if not
-    return True
+
+
+
+def instantiate_modules():
+    global manager
+    manager = XschedulyManager()
 
 if __name__ == "__main__":
     main()
--- src/xscheduly_manager/xscheduly_manager.py
+++ src/xscheduly_manager/xscheduly_manager.py
@@ -1,7 +1,27 @@
+import time
+
 def init_manager():
     pass
+
+def validate_csv_file(file_path):
+    # TODO("write logic ")
+    return True
 
 class XschedulyManager:
     def __init__(self):
         init_manager()
-        
+
+
+    def verify_file_format(self, file_path):
+        print("Checking file format", end="")
+        for _ in range(10):
+            print(".", end="", flush=True)
+            time.sleep(0.025)
+
+        if validate_csv_file(file_path):
+            return "OK"
+        else:
+            return "FORMAT_ERROR"
+
+
+
