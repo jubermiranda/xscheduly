8cc01f73f691e0e72fe81e10613f44d1d4c422e9
user <email>
2023-10-15 11:19:55 -0400

    talbe_extractor, search for tables in odt file

--- src/xscheduly_manager/odt_table_extractor.py
+++ src/xscheduly_manager/odt_table_extractor.py
@@ -5,12 +5,31 @@ tables found inside odt fiel
 """
 import os
 import sys
-import docx
+import zipfile
+import xml.etree.ElementTree as ET
 
 class OdtTableExtractor:
     def __init__(self):
         pass
 
     def check_content_format(file_path):
+        try:
+            with zipfile.ZipFile(file_path, 'r') as odt:
+
+                content_xml = odt.read('content.xml')
+                root = ET.fromstring(content_xml)
+
+                namespaces = {
+                    'text': 'urn:oasis:names:tc:opendocument:xmlns:text:1.0',
+                    'table': 'urn:oasis:names:tc:opendocument:xmlns:table:1.0',
+                }
+
+                table_elements = root.findall('.//table:table', namespaces)
+
+                return len(table_elements) > 0
+
+        except Exception as e:
+            print(f"Error while checking for tables: {str(e)}")
+            return False
 
 
