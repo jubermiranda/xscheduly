f05de349d6ad0e29af1954edb85cf6ee524b0fb3
user <email>
2023-10-15 07:30:43 -0400

    changings in progress, tests may fail here

--- src/xscheduly_manager/xscheduly_manager.py
+++ src/xscheduly_manager/xscheduly_manager.py
@@ -12,27 +12,55 @@ Usage:
 import time
 import os
 
+from xscheduly_manager import odt_table_extractor
+
 def animation_point(n):
     for _ in range(n):
         print(".", end="", flush=True)
         time.sleep(0.025)
 
+def print_animations(messages, ms_delay):
+    for m in messages:
+        print(m)
+        animation_point(ms_delay)
+
 def is_csv_file(self, file_path):
-    return os.path.isfile(file_path) and file_path.endswith('.csv')
+    if os.path.isfile(file_path) and file_path.endswith('.csv'):
+        return
+    else:
+        raise Exception(" file name error ")
 
 def get_file_content(self, file_path, colum_numbers):
-    if is_csv_file(self, file_path):
-        animation_point(5)
-        print(" File exist [OK]")
-        return True
-    else:
-        return False
+    result = []
+    message = []
+
+    try:
+        is_csv_file(self, file_path)
+        message.append("[File exist]")
+
+        try:
+                result = odt_table_extractor.get_tables(file_path)
+                #all current check ok here after .get_tables
+                #
+                message.append(f"[{result.size()} table(s) found]")
+
+        except Exception as e:
+                message.append("[File Content Format Error]")
+
+    except Exception as e:
+        message.append("[File not exist, or not a odt file]")
+
+    #coment line below to not print animations:
+    print_animations(messages)
+
+    return result
+
 
 class XschedulyManager:
 
     def verify_file_format(self, file_path, colum_numbers):
-        print("[Checking file]", end="")
-        animation_point(5)
+        # comment line below to remove animation
+        print_animations("[Checking file]", 3)
 
         return  get_file_content(self, file_path, colum_numbers)
 
